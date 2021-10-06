e29eb622af7eedc409122a8230a1d7b59af17b0e
ceaserinc <69359273+Ai-magic@users.noreply.github.com>
2021-10-06 10:00:47 -0500

    make thiong

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function buttonPress(){
-  alert("You Pressed a button") 
+const number1 = document.getElementById("number1")
+const number2 = document.getElementById("number2")
+const operation = document.getElementById("operation")
+
+function buttonPress() {
+  if (operation.value == "Multiply") {
+    alert(number1 * number2)
+  }
+}
+
+function calculate() {
+
 }
